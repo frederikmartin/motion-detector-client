3304d8cafcaf558d45cd4bb1f0ecaf42971abca2
Frederik Martin <frederik.martin@protonmail.com>
2019-08-03 13:48:29 +0200

    Add readme and update parameter usage

---
+++ README.md
@@ -0,0 +1,32 @@
+# Motion Detector Client
+
+The motion detector client requests a [Philips Hue Motion Sensor](https://www2.meethue.com/en-us/p/hue-motion-sensor/046677473389) and sends motion notifications to an AWS Lambda for home automation purposes.
+
+The client can e.g. be called within a cronjob using `crontab` to frequently check for motion:
+
+```bash
+# Motion Detector, runs every minute
+* * * * * LAMBDA_API_KEY=<LAMBDA_API_KEY> python3 /<PATH_TO_PROJECT>/motion-detector-client/motion_detector_client.py <HUE_BRIDGE_IP> <HUE_USER_ID> <HUE_MOTION_SENSOR_ID> <AWS_LAMBDA_NOTIFICATION_WEBHOOK_URL>
+```
+
+## Requirements
+
+Python >= 3.5
+
+## Usage
+
+```bash
+usage: motion_detector_client.py [-h] [--log_path LOG_PATH]
+                                 hue_bridge hue_user hue_motion_sensor
+                                 notify_hook
+
+positional arguments:
+  hue_bridge           IP address for Philips Hue bridge
+  hue_user             User id for Philips Hue bridge
+  hue_motion_sensor    Motion sensor id
+  notify_hook          URL for notification request
+
+optional arguments:
+  -h, --help           show this help message and exit
+  --log_path LOG_PATH  Filepath for logging
+```
--- motion_detector_client.py
+++ motion_detector_client.py
@@ -5,9 +5,7 @@ import sys
 import argparse
 from datetime import datetime, timedelta
 
-logger = helpers.Logger(True, '/tmp/logs/motion_detector_client.log')
-
-def detect_motion(bridge, user, motion_sensor):
+def detect_motion(bridge, user, motion_sensor, logger):
     logger.info('Request motion sensor')
     hue_client = motion_detector.HueClient(base_url=bridge, user=user, motion_sensor=motion_sensor, logger=logger)
     motion_detected, last_updated = hue_client.query_sensor_data()
@@ -15,28 +13,32 @@ def detect_motion(bridge, user, motion_sensor):
     logger.info('Last updated: {}'.format(last_updated))
     return motion_detected, last_updated
 
-def notify_user(notify_hook):
+def notify_user(notify_hook, logger):
     logger.info('Request notification hook')
     lambda_client = motion_detector.LambdaClient(base_url=notify_hook, logger=logger)
     response = lambda_client.notify_user()
     logger.info('Notification: {}'.format(response['status']))
 
 def main(args):
+    logger = helpers.Logger(True, args.log_path)
+
     motion_detected, last_updated = detect_motion(
         bridge=args.hue_bridge,
         user=args.hue_user,
-        motion_sensor=args.hue_motion_sensor
+        motion_sensor=args.hue_motion_sensor,
+        logger=logger
     )
     curr_timestamp = datetime.utcnow()
     if motion_detected or (curr_timestamp - last_updated) < timedelta(minutes=1):
         logger.info('Motion alert triggered')
-        notify_user(args.notify_hook)
+        notify_user(args.notify_hook, logger=logger)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-b', '--hue_bridge', help='IP address for Philips Hue bridge', required=True)
-    parser.add_argument('-u', '--hue_user', help='User id for Philips Hue bridge', required=True)
-    parser.add_argument('-i', '--hue_motion_sensor', help='Motion sensor id', required=True)
-    parser.add_argument('-n', '--notify_hook', help='URL for notification request', required=True)
+    parser.add_argument('hue_bridge', help='IP address for Philips Hue bridge')
+    parser.add_argument('hue_user', help='User id for Philips Hue bridge')
+    parser.add_argument('hue_motion_sensor', help='Motion sensor id')
+    parser.add_argument('notify_hook', help='URL for notification request')
+    parser.add_argument('--log_path', help='Filepath for logging', default='/tmp/logs/motion_detector_client.log')
     args = parser.parse_args()
     main(args)
